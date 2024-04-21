42de31fc46591e0da4735418fe00fe4e55b53f4a
Abdulwahab Nasir <weybansky@gmail.com>
2024-04-21 10:30:53 +0100

    progress bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
+<script setup>
+import { onMounted, getCurrentInstance } from "vue";
+import { useRouter } from "vue-router";
+
+const internalInstance = getCurrentInstance();
+const router = useRouter();
+const progressBarProp =
+  internalInstance.appContext.config.globalProperties.$Progress;
+
+progressBarProp.start();
+
+onMounted(() => {
+  const origin = window.location.origin;
+
+  if (origin.includes("business.bimpe.ai")) {
+    window.location.href = "https://bimpe.ai/restaurant";
+    return;
+  }
+
+  if (origin.includes("restaurant.bimpe.ai")) {
+    window.location.href = "https://bimpe.ai/restaurant";
+    return;
+  }
+
+  progressBarProp.finish();
+});
+
+router.beforeEach((to, from, next) => {
+  //  does the page we want to go to have a meta.progress object
+  if (to.meta.progress !== undefined) {
+    let meta = to.meta.progress;
+    // parse meta tags
+    progressBarProp.parseMeta(meta);
+  }
+  //  start the progress bar
+  progressBarProp.start();
+  //  continue to next page
+  next();
+});
+
+router.afterEach((to, from) => {
+  //  finish the progress bar
+  progressBarProp.finish();
+});
+</script>
+
 <template>
   <v-app>
     <v-main>
       <router-view />
+      <vue-progress-bar></vue-progress-bar>
     </v-main>
   </v-app>
 </template>
-
-<script setup>
-  //
-</script>
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,10 +8,26 @@
 import vuetify from './vuetify'
 import pinia from '@/stores'
 import router from '@/router'
+import VueProgressBar from '@aacassandra/vue3-progressbar'
 
-export function registerPlugins (app) {
+const vueProgressOptions = {
+  color: "#0099ff", //"#FF614D"
+  failedColor: "#874b4b",
+  thickness: "5px",
+  transition: {
+    speed: "0.2s",
+    opacity: "0.6s",
+    termination: 300,
+  },
+  autoRevert: true,
+  location: "top",
+  inverse: false,
+}
+
+export function registerPlugins(app) {
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
+    .use(VueProgressBar, vueProgressOptions)
 }
